48dc539b1f19a9a322290ab7d912ce0f2e041093
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-18 22:45:19 +0100

    feat: Add TypeScript definitions for next-auth session and user interfaces

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,21 @@
+import { Session } from 'next-auth';
+
+declare module 'next-auth' {
+	interface Session {
+		user: {
+			username: string | undefined;
+			name?: string | null;
+			email?: string | null;
+			image?: string | null;
+			id: string;
+			twoFactorComplete?: boolean;
+		};
+	}
+	interface User {
+		username: string | null;
+		name: string;
+		email: string;
+		image?: string;
+		twoFactorComplete?: boolean;
+	}
+}
