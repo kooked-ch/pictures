992c06af3f36f3db01caf6758ace93474c6ec723
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-18 23:54:46 +0100

    refactor: Simplify NextAuth route handling by exporting GET and POST functions directly

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,12 +2,10 @@ import type { NextRequest } from 'next/server';
 import NextAuth from 'next-auth';
 import { authOptions } from '@/lib/auth';
 
-interface RouteHandlerContext {
-	params: { nextauth: string[] };
-}
-
-async function auth(req: NextRequest, context: RouteHandlerContext) {
+export async function GET(req: NextRequest, context: any) {
 	return await NextAuth(req, context, authOptions);
 }
 
-export { auth as GET, auth as POST };
+export async function POST(req: NextRequest, context: any) {
+	return await NextAuth(req, context, authOptions);
+}
